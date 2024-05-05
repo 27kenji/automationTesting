e1445cc7b5757dede693b52ef26e875d77c1d63d
27kenji <74094339+27kenji@users.noreply.github.com>
2024-05-05 17:41:14 +0300

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,19 @@
+public class MainClass {
+    public int getLocalNumber(int number) {
+        return number;
+    }
+
+    private int class_number = 20;
+
+    private String class_string = "Hello, world";
+
+    public int getClassNumber() {
+        return class_number;
+    }
+
+    public String getClassString() {
+        return class_string;
+    }
+
+
+}
