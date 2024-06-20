d8516510f9b4615ecfc4d5392c5de7ee262d44dd
27kenji <74094339+27kenji@users.noreply.github.com>
2024-06-20 20:42:18 +0300

    Add files via upload

---
+++ CoreTestCase.java
@@ -0,0 +1,50 @@
+package lib;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import junit.framework.TestCase;
+import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+import java.time.Duration;
+
+public class CoreTestCase extends TestCase {
+    protected AppiumDriver driver;
+    private static String AppiumURL = "http://127.0.0.1:4723/";
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("appium:deviceName", "AndroidTestDevice");
+        capabilities.setCapability("appium:platformVersion", "13.0");
+        capabilities.setCapability("appium:automationName", "UiAutomator2");
+        capabilities.setCapability("appium:appPackage", "org.wikipedia");
+        capabilities.setCapability("appium:appActivity", ".main.MainActivity");
+        capabilities.setCapability("appium:app", "C:\\Users\\user\\Desktop\\JavaAppiumAutomation\\JavaAppiumAutomation\\apks\\wikipedia_android.apk");
+
+        driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+        this.rotateScreenPortrait();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        driver.quit();
+    }
+
+    protected void rotateScreenPortrait() {
+        driver.rotate(ScreenOrientation.PORTRAIT);
+    }
+
+    protected void rotateScreenLandscape() {
+        driver.rotate(ScreenOrientation.LANDSCAPE);
+    }
+
+    protected void backgroundApp(int seconds) {
+        driver.runAppInBackground(Duration.ofSeconds(seconds));
+    }
+
+}
